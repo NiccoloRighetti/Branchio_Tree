e39fbe85df531867c66c642cf3546c85de9bad0d
Niccolò Righetti <niccolo.righetti@studio.unibo.it>
2023-12-20 13:14:33 +0100

    Create topology_tester.sh

---
+++ topology_tester.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pruned cladograms based on genetree species list
+python3 pruner.py tree_uncalib.tree
+
+for i in *_cladogram.nwk; do
+  seq_id = $(basename -s _cladogram.nwk "$i")
+  seq_path = $(/home/STUDENTI/niccolo.righetti/Branchio_2.0/08_node_calibration/data/busco_fna/trimmed_fna/"$seq_id")
+  # Genetree constrained
+  iqtree2 -s "$seq_path" -g "$i" --prefix ./"$seq_id"_constr
+  # Topology test gene by gene
+  # lista coi due alberi
+  # topology test
