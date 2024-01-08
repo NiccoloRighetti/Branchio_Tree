0bff09051c53c9fe7174170959e171bc232ab78b
Mirko Martini <131965986+MirkMart@users.noreply.github.com>
2024-01-08 10:52:22 +0100

    Update topology_tester.sh

--- topology_tester.sh
+++ topology_tester.sh
@@ -7,6 +7,7 @@ for i in *_cladogram.nwk; do
   seq_id = $(basename -s _cladogram.nwk "$i")
   seq_path = $(/home/STUDENTI/niccolo.righetti/Branchio_2.0/08_node_calibration/data/busco_fna/trimmed_fna/"$seq_id")
   # Genetree constrained
+  best_model=$(grep "Best-fit model" "$OG".log | sed -E 's/(.+:) (.[^ ]+) (.+$)/\2/')
   iqtree2 -s "$seq_path" -g "$i" --prefix ./"$seq_id"_constr
   # Topology test gene by gene
   cat "$seq_id".treefile "$seq_id"_constr.treefile > "$seq_id".treels
