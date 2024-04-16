97c68e4ba4dba95014fd037f30c6e113e29b97b1
Niccolò Righetti <niccolo.righetti@studio.unibo.it>
2024-04-16 14:12:39 +0200

    jk

---
+++ Branchio_rerun_06_04_2024/mcmctree_jackknife/mcmctreelauncher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Loop through the No folders
+for i in {1..4}; do
+    no_folder="No$i"
+
+    # Loop through run1 and run2 subfolders
+    for subfolder in run1 run2; do
+        subfolder_path="$no_folder/$subfolder"
+        
+        # Change to the subfolder
+        cd "$subfolder_path" || exit
+
+        # Execute mcmctree for all *.ctl files
+        mcmctree *.ctl &
+
+        # Change back to the original directory
+        cd - || exit
+    done
+done
