4ce18a789148cbfb60aae9eae602f4235ef6d8bd
Filippo Nicolini <filippo.nicolini6@unibo.it>
2024-08-19 15:25:55 +0200

    update figure 2

--- figures/figure2/figure_2.R
+++ figures/figure2/figure_2.R
@@ -18,7 +18,7 @@ tibble_to_plot <- data %>%
   mutate(clade = stringr::str_replace(clade, "_", " "))
 
 tibble_to_plot$clade <- factor(tibble_to_plot$clade, levels = unique(c("Branchiopoda (1)", "Anostraca (2)", "Phyllopoda", "Notostraca (3)", "Diplostraca", "Onychocaudata", "Spinicaudata", "Cladoceromorpha", "Cyclestherida", "Cladocera (4)", "Anomopoda", "Onycopoda")))
-tibble_to_plot$study <- factor(tibble_to_plot$study, levels = unique(c("sun2016", "uozomi2021", "vanDamme2022", "bernot2023", "thisWork")))
+tibble_to_plot$study <- factor(tibble_to_plot$study, levels = unique(c("sun2016", "uozomi2021", "xu2021", "vanDamme2022", "bernot2023", "thisWork")))
 
 tibble_to_plot
 
@@ -35,21 +35,21 @@ plot <- ggplot(tibble_to_plot, aes(x = clade, y = date)) +
                 position = position_dodge(width = 0.5)) +
   
   scale_color_manual("Study",
-                     labels = c("Sun et al., 2016\n(r8s)", "Uozomi et al., 2021\n(MEGA X)", "Van Damme et al., 2022\n(BEAST)", "Bernot et al., 2023\n(MCMCtree)", "This work\n(MCMCtree)"),
-                     values = c("#c08fe1", "#9dceda", "#a7d7b1", "#f7d57f", "#f2999d")) +
+                     labels = c("Sun et al., 2016\n(r8s)", "Uozomi et al., 2021\n(MEGA X)", "Xu et al., 2021\n(BEAST)","Van Damme et al., 2022\n(BEAST)", "Bernot et al., 2023\n(MCMCtree)", "This work\n(MCMCtree)"),
+                     values = c("#c08fe1", "#92b5f1","#9dceda", "#a7d7b1", "#f7d57f", "#f2999d")) +
   
   
   ggnewscale::new_scale_color() +
   geom_point(aes(col = study), size = 1.5, position = position_dodge(width = 0.5)) +
   
-  annotate("text", label = c("a", "b"), hjust = 0.1, size = 3, col = c("#ae4dee", "#00c7eb"),
+  annotate("text", label = c("a", "b"), hjust = 0.1, size = 3, col = c("#ae4dee", "#2069e7"),
            x = c(2, 4),
            y = c(tibble_to_plot$date[tibble_to_plot$clade == "Anostraca (2)" & tibble_to_plot$study == "sun2016"],
                  tibble_to_plot$date[tibble_to_plot$clade == "Notostraca (3)" & tibble_to_plot$study == "uozomi2021"])) +
   
   scale_color_manual("Study",
-                     labels = c("Sun et al., 2016\n(r8s)", "Uozomi et al., 2021\n(MEGA X)", "Van Damme et al., 2022\n(BEAST)", "Bernot et al., 2023\n(MCMCtree)", "This work\n(MCMCtree)"),
-                     values = c("#ae4dee", "#00c7eb", "#0cda53", "#ffa700", "#f41d27")) +
+                     labels = c("Sun et al., 2016\n(r8s)", "Uozomi et al., 2021\n(MEGA X)", "Xu et al., 2021\n(BEAST)","Van Damme et al., 2022\n(BEAST)", "Bernot et al., 2023\n(MCMCtree)", "This work\n(MCMCtree)"),
+                     values = c("#ae4dee", "#2069e7","#00c7eb", "#0cda53", "#ffa700", "#f41d27")) +
   
   scale_x_discrete(expand = c(0, 0)) +
   
@@ -64,14 +64,15 @@ plot <- ggplot(tibble_to_plot, aes(x = clade, y = date)) +
         panel.grid.major.y = element_blank(),
         panel.grid.major.x = element_blank(),
         axis.line = element_line(linewidth = 0.5),
-        axis.text.x = element_text(angle = 45, hjust = 1, size = 9),
-        axis.text.y = element_text(size = 9),
-        axis.title = element_text(size = 9, face = "bold"),
-        legend.position = "top",
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 7),
+        axis.text.y = element_text(size = 7),
+        axis.title = element_text(size = 7, face = "bold"),
+        legend.position = "right",
         legend.background = element_rect(fill = 'transparent', color = NA),
-        legend.key.size = unit(0.4, "cm"),
-        legend.text = element_text(size = 7),
-        legend.title = element_text(size = 8, face = "bold")
+        legend.key.size = unit(0.3, "cm"),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 7, face = "bold"),
+        legend.key.spacing.y = unit(0.2, 'cm')
         )
 
 plot
