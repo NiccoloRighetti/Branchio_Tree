a5c5dce49b4e3a55fc1b909141a02904ee86df24
Filippo Nicolini <filippo.nicolini6@unibo.it>
2024-08-20 12:13:57 +0200

    Update figure_2.R

--- figures/figure2/figure_2.R
+++ figures/figure2/figure_2.R
@@ -79,6 +79,73 @@ plot <- ggplot(tibble_to_plot, aes(x = clade, y = date)) +
 
 plot
 
+plot_inverted <- ggplot(tibble_to_plot, aes(x = date, y = clade)) +
+  
+  
+  
+  annotate("rect", fill = "#f6f6f6",
+           xmin = c(590,490,420,300,200,66),
+           xmax = c(540,440,360,250,140,23),
+           ymin = -Inf,
+           ymax = +Inf) +
+  
+  geom_errorbar(aes(xmin = lowerCI, xmax = upperCI, y = clade, col = study),
+                width = 0.4, linewidth = 0.4, lineend = "round",
+                position = position_dodge(width = 0.3)) +
+  
+  scale_color_manual("Study",
+                     labels = c("Sun et al., 2016\n(r8s)", "Uozomi et al., 2021\n(MEGA X)", "Xu et al., 2021\n(BEAST)","Van Damme et al., 2022\n(BEAST)", "Bernot et al., 2023\n(MCMCtree)", "This work\n(MCMCtree)"),
+                     values = c("#c08fe1", "#92b5f1","#9dceda", "#a7d7b1", "#f7d57f", "#f2999d")) +
+  
+  
+  ggnewscale::new_scale_color() +
+  geom_point(aes(col = study), size = 1.5, position = position_dodge(width = 0.3)) +
+  
+  annotate("text", label = c("a", "b"), vjust = 0.1, hjust = 1.3, size = 3, col = c("#ae4dee", "#2069e7"),
+           x = c(tibble_to_plot$date[tibble_to_plot$clade == "Anostraca (2)" & tibble_to_plot$study == "sun2016"],
+                 tibble_to_plot$date[tibble_to_plot$clade == "Notostraca (3)" & tibble_to_plot$study == "uozomi2021"]),
+           y = c(11, 9)) +
+  
+  scale_color_manual("Study",
+                     labels = c("Sun et al., 2016\n(r8s)", "Uozomi et al., 2021\n(MEGA X)", "Xu et al., 2021\n(BEAST)","Van Damme et al., 2022\n(BEAST)", "Bernot et al., 2023\n(MCMCtree)", "This work\n(MCMCtree)"),
+                     values = c("#ae4dee", "#2069e7","#00c7eb", "#0cda53", "#ffa700", "#f41d27")) +
+  
+  scale_y_discrete(limits = rev) +
+  scale_x_reverse(expand = c(0, 0), limits = c(590, 0), breaks = c(0, 23, 66, 140, 200, 250, 300, 360, 420, 440, 490, 540, 590),
+                  position = "top") +
+  
+  ylab("Clade") +
+  xlab("Years (Ma)") +
+  
+  coord_cartesian(clip = "off") +
+  
+  theme_minimal() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        # panel.grid.major.y = element_line(color = "#818181", linetype = "longdash"),
+        axis.line = element_line(linewidth = 0.5, color = "#4c4c4c"),
+        axis.text.y = element_text(angle = 0, size = 7, color = "#4c4c4c"),
+        axis.text.x = element_text(size = 7, color = "#4c4c4c"),
+        axis.title = element_text(size = 7, face = "bold", color = "#4c4c4c"),
+        axis.ticks.x = element_line(color = "#4c4c4c"),
+        legend.position = "right",
+        legend.background = element_rect(fill = 'transparent', color = NA),
+        legend.key.size = unit(0.3, "cm"),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 7, face = "bold"),
+        legend.key.spacing.y = unit(0.2, 'cm')
+  )
+
+plot_inverted
+
+
 ggsave("figure_2.png",
        plot = plot, device = "png",
        dpi = 300, height = 4, width = 7, units = ("in"), bg = 'white')
+
+ggsave("figure_2_inverted.png",
+       plot = plot_inverted, device = "png",
+       dpi = 300, height = 4, width = 7, units = ("in"), bg = 'white')
+
